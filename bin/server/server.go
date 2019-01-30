de1e73d3ef63e583b81bb04251b719e88f04f5df
michalnov <32653270+michalnov@users.noreply.github.com>
2019-01-30 21:02:44 +0100

    added templates, configuration for host addres, serving web templates (#9)
    * basic structure of main security JS
    
    * working on mainSec.js adding functions for setup connection
    
    * work on server side crypto package
    
    * created basics of user authetication
    
    * remove redundant key from main
    
    * start creating templates working on structures
    
    * added Envelop structure
    
    * finishing key/new resource
    
    * added help prints
    
    * fixing token and server pem
    
    * updating mainSec.js add envelops
    
    * updating functions of key exchange (on  client and also on server side)
    
    * bugfix mainSec.js callng of setup() function moved at the end of file
    
    * bugfix mainSec.js buildEnvelop fixed
    
    * bugfix composition of xml file (mainSec.js buildEnvelop())
    
    repeated seting of response code (core/cryptoHandler.go)
    
    * update configuration
    
    * added templates

--- bin/server/server.go
+++ bin/server/server.go
@@ -1,28 +1,75 @@
 package server
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/michalnov/SovyGo/bin/server/core"
+	"github.com/michalnov/SovyGo/bin/server/modules/persistance"
+
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{}
 
 //Server structure that hold all parts of application
 type Server struct {
 	r           *mux.Router
 	degradation chan int
+	state       persistance.Persistance
+	core        core.Core
 }
 
 //SetupServer prepare new server structure
 func (s *Server) SetupServer(degradation chan int) error {
 	fmt.Println("Creating server")
-	out := Server{}
-	out.degradation = degradation
-
+	s.degradation = degradation
+	s.state = persistance.NewPersistance()
 	return nil
 }
 
 //StartServer create routes and execute http.listenAndServe
 func (s *Server) StartServer() error {
-
+	var err error
+	s.core, err = core.NewCore()
+	if err != nil {
+		return err
+	}
+	s.r = mux.NewRouter()
+	s.r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web_files/test.html")
+	})
+	s.r.HandleFunc("/home", s.core.HomeHandler)
+	s.r.HandleFunc("/register", s.core.RegisterHandler)
+	s.r.HandleFunc("/login", s.core.LoginHandler)
+	s.r.HandleFunc("/test", s.core.TestHandler)
+	s.r.HandleFunc("/key/new/", func(w http.ResponseWriter, r *http.Request) {
+		core.NewKey(w, r, &s.state)
+	})
+	s.r.HandleFunc("/key/aes/", func(w http.ResponseWriter, r *http.Request) {
+		core.ImportAESKey(w, r, &s.state)
+	})
+	s.r.HandleFunc("/off/1234", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("shutdown"))
+		s.degradation <- 0
+	})
+	s.r.HandleFunc("/hello", notImplemented)
+	http.Handle("/", s.r)
+	log.Fatal(http.ListenAndServe(s.core.Config.Server.Port, handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(s.r)))
 	return nil
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	http.ServeFile(w, r, "web_files/test.html")
+}
+
+func notImplemented(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("not implemented yet"))
 }
